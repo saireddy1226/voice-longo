8be80d004c4d918b106e5bca70393a2b420990a6
saireddy1226 <saireddyodeti1226@gmail.com>
2026-04-05 13:37:49 +0530

    update config for production

--- voice-lingo/frontend/js/config.js
+++ voice-lingo/frontend/js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    SERVER_URL: "https://192.168.0.110:8000",
-    WS_URL: "wss://192.168.0.110:8000"
+    SERVER_URL: "https://voice-lingo.onrender.com",
+    WS_URL: "wss://voice-lingo.onrender.com"
 };
